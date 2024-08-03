a6b2f45f11ed70b790b18324510d6db05413ba9d
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-03 16:01:34 +0100

    setup: add index.js to export and import assets easily

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+import contact from "./contact.png";
+import cta from "./cta.png";
+import header from "./header.png";
+import logoBlack from "./logo-black.png";
+import logoLight from "./logo-light.png";
+
+// Brands
+import amazon from "./brands/amazon.png";
+import dribbble from "./brands/dribbble.png";
+import hubspot from "./brands/hubspot.png";
+import netflix from "./brands/netflix.png";
+import notion from "./brands/notion.png";
+import zoom from "./brands/zoom.png";
+
+// Services
+import advertising from "./services/advertising.png";
+import analyticsTracking from "./services/analytics-tracking.png";
+import contentCreation from "./services/content-creation.png";
+import emailMarketing from "./services/email-marketing.png";
+import seo from "./services/seo.png";
+import socialMediaMarketing from "./services/social-media-marketing.png";
+
+// Team members
+import brianWilliams from "./team/brian-williams.png";
+import emilyJohnson from "./team/emily-johnson.png";
+import janeDoe from "./team/jane-doe.png";
+import johnSmith from "./team/john-smith.png";
+import michaelBrown from "./team/michael-brown.png";
+import sarahKim from "./team/sarah-kim.png";
+
+export {
+    contact, cta, header, logoBlack, logoLight, amazon, dribbble, hubspot, netflix, notion, zoom, advertising, analyticsTracking, contentCreation, emailMarketing, seo, socialMediaMarketing, brianWilliams, emilyJohnson, janeDoe, johnSmith, michaelBrown, sarahKim
+}
