6d8ceb87911523003ca78e014a5b1e81fb01b849
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-04 03:07:34 +0100

    setup: Create reusable components

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Button from "./Button";
+import Case from "./Case";
+import Feedback from "./Feedback";
+import Heading from "./Heading";
+import Member from "./Member";
+import ServiceCard from "./ServiceCard";
+import Step from "./Step";
+
+export { Button, Case, Feedback, Heading, Member, ServiceCard, Step }
