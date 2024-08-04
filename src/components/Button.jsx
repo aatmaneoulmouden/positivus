57d69c75934b94fef9e82b6aebdd1edb873e2c7d
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-04 10:05:26 +0100

    build: Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,17 @@
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ role = "button", type, scrollto, label }) => {
+  let buttonClassNames;
+  if (type === "primary") {
+    buttonClassNames = "text-black border-green bg-green";
+  } else if(type === "secondary") {
+    buttonClassNames = "text-white border-dark bg-dark";
+  } else {
+    buttonClassNames = "border-dark text-black";
+  }
+  return (
+    <button type={role} className={`border rounded-[14px] py-4 px-9 ${buttonClassNames}`}>
+      {label}
+    </button>
+  );
 };
 
 export default Button;
