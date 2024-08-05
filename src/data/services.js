36f1ead8aa24fd26f3d7f8f087d8898c4b08d6df
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-05 12:01:21 +0100

    fix: Update data structure and add new data

--- src/data/services.js
+++ src/data/services.js
@@ -2,34 +2,70 @@ import { advertising, analyticsTracking, contentCreation, emailMarketing, seo, s
 
 const services = [
     {
-        title: "Search Engine Optimization",
+        title: {
+            full: "Search Engine Optimization",
+            firstPart: "Search Engine",
+            secondPart: "Optimization",
+        },
         image: seo,
         link: "/",
+        bgColor: "bg-gray",
+        highlightColor: "bg-green",
     },
     {
-        title: "Pay-per-click Advertising",
+        title: {
+            full: "Pay-per-click Advertising",
+            firstPart: "Pay-per-click",
+            secondPart: "Advertising",
+        },
         image: advertising,
         link: "/",
+        bgColor: "bg-green",
+        highlightColor: "bg-gray",
     },
     {
-        title: "Social Media Marketing",
+        title: {
+            full: "Social Media Marketing",
+            firstPart: "Social Media",
+            secondPart: "Marketing",
+        },
         image: socialMediaMarketing,
         link: "/",
+        bgColor: "bg-dark",
+        highlightColor: "bg-gray",
     },
     {
-        title: "Email Marketing",
+        title: {
+            full: "Email Marketing",
+            firstPart: "Email",
+            secondPart: "Marketing",
+        },
         image: emailMarketing,
         link: "/",
+        bgColor: "bg-gray",
+        highlightColor: "bg-green",
     },
     {
-        title: "Content Creation",
+        title: {
+            full: "Content Creation",
+            firstPart: "Content",
+            secondPart: "Creation",
+        },
         image: contentCreation,
         link: "/",
+        bgColor: "bg-green",
+        highlightColor: "bg-gray",
     },
     {
-        title: "Analytics and Tracking",
+        title: {
+            full: "Analytics and Tracking",
+            firstPart: "Analytics and",
+            secondPart: "Tracking",
+        },
         image: analyticsTracking,
         link: "/",
+        bgColor: "bg-dark",
+        highlightColor: "bg-green",
     },
 ];
 
