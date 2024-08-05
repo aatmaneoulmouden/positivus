06172a3a2422549a5c26d25a67ab2282c64edb8f
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-05 19:36:09 +0100

    build: Case card

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -1,5 +1,15 @@
-const Case = () => {
-  return <div>Case</div>;
+import { FiArrowUpRight } from "react-icons/fi";
+
+const Case = ({ description, link, additionalClasses }) => {
+  return (
+    <article className={`flex-1 flex flex-col gap-5 ${additionalClasses}`}>
+      <p className="text-lg">{description}</p>
+      <a href={link} className="flex justify-start items-center gap-2 text-green">
+        Learn more
+        <FiArrowUpRight fontSize={28} />
+      </a>
+    </article>
+  );
 };
 
 export default Case;
