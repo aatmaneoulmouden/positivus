9a251b784ba15da8a233df186c9e3974641f04bd
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-05 09:09:57 +0100

    build: Partners section web and mobile + installing Splide carousel

--- src/sections/Partners.jsx
+++ src/sections/Partners.jsx
@@ -1,5 +1,74 @@
+import { partners } from "../data";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
+
+import "@splidejs/react-splide/css";
+
 const Partners = () => {
-  return <div>Partners</div>;
+  const displayPartners = partners.map((partner, index) => (
+    <img
+      key={index}
+      src={partner.logo}
+      alt={partner.name}
+      className="grayscale hover:grayscale-0 cursor-pointer"
+    />
+  ));
+
+  const displayFirstTreePartners = partners
+    .slice(0, 3)
+    .map((partner, index) => (
+      <SplideSlide key={index}>
+        <img
+          src={partner.logo}
+          alt={partner.name}
+          className="grayscale hover:grayscale-0 cursor-pointer w-[122px]"
+        />
+      </SplideSlide>
+    ));
+
+  const displayLastTreePartners = partners.slice(-3).map((partner, index) => (
+    <SplideSlide key={index}>
+      <img
+        src={partner.logo}
+        alt={partner.name}
+        className="grayscale hover:grayscale-0 cursor-pointer w-[122px]"
+      />
+    </SplideSlide>
+  ));
+
+  const splideOptions = {
+    type: "loop",
+    arrows: false,
+    pagination: false,
+    fixedWidth: "122px",
+    gap: "38px",
+    speed: 500,
+  };
+
+  return (
+    <section className="section">
+      <div className="container">
+        <div className="desktop-logos hidden lg:flex justify-between items-center flex-wrap">
+          {displayPartners}
+        </div>
+      </div>
+      <div className="mobile-logos block lg:hidden">
+        <div className="mb-5">
+          <Splide extensions={{ AutoScroll }} options={splideOptions}>
+            {displayFirstTreePartners}
+          </Splide>
+        </div>
+        <div>
+          <Splide
+            extensions={{ AutoScroll }}
+            options={{ direction: "rtl", ...splideOptions }}
+          >
+            {displayLastTreePartners}
+          </Splide>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Partners;
