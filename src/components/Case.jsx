e86d387a37881fcdab26723ceb1c77ce191309e8
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-06 10:26:24 +0100

    build: Case Studies section and Case component mobile version

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -1,13 +1,33 @@
 import { FiArrowUpRight } from "react-icons/fi";
 
-const Case = ({ description, link, additionalClasses }) => {
+const Case = ({ description, link, additionalClasses = "" }) => {
   return (
-    <article className={`flex-1 flex flex-col gap-5 ${additionalClasses}`}>
-      <p className="text-lg">{description}</p>
-      <a href={link} className="flex justify-start items-center gap-2 text-green">
-        Learn more
-        <FiArrowUpRight fontSize={28} />
-      </a>
+    <article>
+      {/* In desktop */}
+      <div
+        className={`hidden flex-1 lg:flex flex-col gap-5 ${additionalClasses}`}
+      >
+        <p className="text-lg">{description}</p>
+        <a
+          href={link}
+          className="flex justify-start items-center gap-2 text-green"
+        >
+          Learn more
+          <FiArrowUpRight fontSize={28} />
+        </a>
+      </div>
+
+      {/* In mobile */}
+      <div className="flex lg:hidden flex-col gap-5 px-[50px] py-[42px] rounded-[45px] bg-dark">
+        <p className="text-base text-white">{description}</p>
+        <a
+          href={link}
+          className="flex justify-start items-center gap-2 text-green"
+        >
+          Learn more
+          <FiArrowUpRight fontSize={28} />
+        </a>
+      </div>
     </article>
   );
 };
--- src/sections/CaseStudies.jsx
+++ src/sections/CaseStudies.jsx
@@ -1,8 +1,10 @@
 import { Case, Heading } from "../components";
 import { cases } from "../data";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 const CaseStudies = () => {
-  const displayCases = cases.map((caseStudy, index) => {
+  const displayCasesInDesktop = cases.map((caseStudy, index) => {
     let additionalClasses;
 
     if (index === 0) {
@@ -16,17 +18,38 @@ const CaseStudies = () => {
       <Case key={index} additionalClasses={additionalClasses} {...caseStudy} />
     );
   });
+
+  const displayCasesInMobile = cases.map((caseStudy, index) => (
+    <SplideSlide key={index}>
+      <Case {...caseStudy} />
+    </SplideSlide>
+  ));
   return (
     <section className="section">
-      <div className="container max-w-full">
-        <Heading
-          title="Case Studies"
-          subtitle="Explore Real-Life Examples of Our Proven Digital Marketing Success through Our Case Studies"
-          paragraphWidth="lg:max-w-[580px]"
-        />
-        <div className="content-wrapper mt-10 lg:mt-20 bg-dark text-white py-[70px] px-[60px] rounded-[70px] flex">
-          {displayCases}
+      <div className="in-desktop hidden lg:block">
+        <div className="container max-w-full">
+          <Heading
+            title="Case Studies"
+            subtitle="Explore Real-Life Examples of Our Proven Digital Marketing Success through Our Case Studies"
+            paragraphWidth="lg:max-w-[580px]"
+          />
+          <div className="content-wrapper mt-10 lg:mt-20 bg-dark text-white py-[70px] px-[60px] rounded-[70px] flex">
+            {displayCasesInDesktop}
+          </div>
         </div>
+      </div>
+      <div className="in-mobile block lg:hidden">
+        <Splide
+          options={{
+            type: "loop",
+            gap: "20px",
+            arrows: false,
+            pagination: false,
+            fixedWidth: "360px",
+          }}
+        >
+          {displayCasesInMobile}
+        </Splide>
       </div>
     </section>
   );
