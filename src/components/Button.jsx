ea528145c38c0e40d43b3377bcd46cfd5af7e8bc
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-07 12:09:42 +0100

    fix: Responsive breakpoint

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({ role = "button", type, scrollto, label, classes = "" }) => {
     buttonClassNames = "border-dark text-black";
   }
   return (
-    <button type={role} className={`border rounded-[14px] py-3 lg:py-4 lg:px-9 ${buttonClassNames} ${classes}`}>
+    <button type={role} className={`border rounded-[14px] py-3 md:py-4 md:px-9 ${buttonClassNames} ${classes}`}>
       {label}
     </button>
   );
