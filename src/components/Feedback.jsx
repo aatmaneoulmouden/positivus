9f8a2400f1ad95a97e68988328f22ae5e2cd6740
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-08 00:20:06 +0100

    build: Feedback component

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,5 +1,17 @@
-const Feedback = () => {
-  return <div>Feedback</div>;
+const Feedback = ({ feedback, name, title }) => {
+  return (
+    <article>
+      <div className="comment border border-green rounded-[45px] p-[30px] lg:p-12 relative">
+        <p className="text-base lg:text-lg">{`"${feedback}"`}</p>
+
+        <div className="comment-feedback w-10 h-10 border-r border-b border-green absolute -bottom-[20px] left-24 rotate-45 bg-dark" />
+      </div>
+      <div className="info mt-11 ml-[118px]">
+        <h4 className="text-lg font-medium text-green">{name}</h4>
+        <p className="text-base lg:text-lg">{title}</p>
+      </div>
+    </article>
+  );
 };
 
 export default Feedback;
