d682af7f151ca5838c4d2ea59cba0d7cca83ffe8
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-09 08:59:20 +0100

    build: Testimonials section version 1

--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,5 +1,48 @@
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import { Feedback, Heading } from "../components";
+import { testimonials } from "../data";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  const displayTestimonials = testimonials.map((testimonial, index) => (
+    <SplideSlide key={index} className="w-[330px] lg:w-[606px]">
+      <Feedback {...testimonial} />
+    </SplideSlide>
+  ));
+
+  const splideOptions = {
+    type: "loop",
+    gap: "50px",
+    fixedWidth: "606px",
+    focus: "center",
+    pagination: false,
+    arrows: false,
+    breakpoints: {
+      768: {
+        gap: "30px",
+        fixedWidth: "330px",
+      },
+    },
+  };
+
+  return (
+    <section className="section">
+      <div className="container max-w-full">
+        <Heading
+          title="Testimonials"
+          subtitle="Hear from Our Satisfied Clients: Read Our Testimonials to Learn More about Our Digital Marketing Services"
+          paragraphWidth="lg:max-w-[473px]"
+        />
+        <div className="content-wrapper mt-10 lg:mt-20 bg-dark text-white py-[70px] rounded-[70px]">
+          <Splide hasTrack={false} options={splideOptions}>
+            <SplideTrack>{displayTestimonials}</SplideTrack>
+          </Splide>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
