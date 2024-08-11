0d2d643176d1b5fc2d4248c655f76d0f2fa479fa
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-08-11 02:00:40 +0100

    build: Footer section for mobile

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <li
       key={index}
       data-scrollto={link.section}
-      className="cursor-pointer underline text-lg font-light"
+      className="cursor-pointer lg:underline text-lg font-light"
     >
       {link.title}
     </li>
@@ -67,7 +67,11 @@ const Footer = () => {
                 action=""
                 className="bg-[#292A32] rounded-[14px] flex justify-center items-center gap-5 px-10"
               >
-                <input type="email" className="h-[68px] bg-[#292A32] border border-white rounded-[14px] text-lg px-8 text-white" placeholder="Email" />
+                <input
+                  type="email"
+                  className="h-[68px] bg-[#292A32] border border-white rounded-[14px] text-lg px-8 text-white"
+                  placeholder="Email"
+                />
                 <Button
                   role="submit"
                   type="primary"
@@ -78,13 +82,81 @@ const Footer = () => {
             </div>
             <div className="bottom pt-[50px] border-t border-white flex justify-start items-center gap-10 text-lg text-white">
               <p>&copy; 2024 Positivus. All Rights Reserved.</p>
-              <a href="" className="underline">Privacy Policy</a>
+              <a href="" className="underline">
+                Privacy Policy
+              </a>
             </div>
           </div>
         </div>
       </div>
-      <div className="mobile-footer block lg:hidden bg-dark mt-[60px]">
-        hello
+
+      <div className="mobile-footer block lg:hidden bg-dark mt-[60px] pt-[50px] pb-[30px] px-5">
+        <div className="flex flex-col gap-9 items-center">
+          <a href="">
+            <img
+              src={logoLight}
+              alt="Positivus"
+              className="w-[144px] object-contain lg:w-[220px]"
+            />
+          </a>
+
+          <ul className="flex flex-col items-center gap-[15px] text-white text-base">
+            {displayNavLinks}
+          </ul>
+
+          <div className="contact-info text-center">
+            <h4 className="text-lg font-medium bg-green w-fit px-2 rounded-md mx-auto">
+              Contact us:
+            </h4>
+            <ul className="contact-info-list mt-9 flex flex-col gap-[15px]">
+              <li>Email: info@positivus.com</li>
+              <li>Phone: 555-567-8901</li>
+              <li>
+                Address: 1234 Main St <br /> Moonstone City, Stardust State
+                12345
+              </li>
+            </ul>
+          </div>
+          <form
+            action=""
+            className="bg-[#292A32] rounded-[14px] flex flex-col items-center gap-[22px] p-[30px]"
+          >
+            <input
+              type="email"
+              className="h-[68px] bg-[#292A32] border border-white rounded-[14px] text-base px-6 text-white"
+              placeholder="Email"
+            />
+            <Button
+              role="submit"
+              type="primary"
+              label="Subscribe to news"
+              classes="w-full h-[68px]"
+            />
+          </form>
+          <ul className="flex justify-end items-center gap-5 text-dark text-sm">
+            <li>
+              <a href="" className="social-link">
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li>
+              <a href="" className="social-link">
+                <FaFacebookF />
+              </a>
+            </li>
+            <li>
+              <a href="" className="social-link">
+                <FaTwitter />
+              </a>
+            </li>
+          </ul>
+          <div className="bottom pt-9 border-t border-white flex flex-col gap-[15px] text-base text-white text-center">
+            <p>&copy; 2024 Positivus. All Rights Reserved.</p>
+            <a href="">
+              Privacy Policy
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
